72d1aecd388016a6196c9dd927210de943652320
faisalsadi <fesalsadi@gmail.com>
2022-05-10 20:22:56 +0300

    order updates2

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/OrderBoundary.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/OrderBoundary.java
@@ -1,40 +1,51 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.Flower;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static il.cshaifasweng.OCSFMediatorExample.client.CatalogBoundary.getMap;
 
 public class OrderBoundary {
-    @FXML private AnchorPane Item;
-    @FXML private TextArea txt;
-    @FXML private ChoiceBox<String> str;
-    @FXML private DatePicker datepick;
-    @FXML private Label finalprice;
-    @FXML private Button returnbtn;
-    @FXML private ListView list;
+    @FXML
+    private AnchorPane Item;
+    @FXML
+    private TextArea txt;
+    @FXML
+    private ChoiceBox<String> str;
+    @FXML
+    private DatePicker datepick;
+    @FXML
+    private Label finalprice;
+    @FXML
+    private Button returnbtn;
+    @FXML
+    private Button confirm;
+    @FXML
+    private ListView list;
 
-    @FXML private ChoiceBox<String> payment;
+    @FXML
+    private ChoiceBox<String> payment;
 
     private ArrayList<String> liststr = new ArrayList<String>();
     private ArrayList<String> cartlist = new ArrayList<String>();
-    HashMap<Flower,Integer> map=CatalogBoundary.getMap();
+    HashMap<Flower, Integer> map = CatalogBoundary.getMap();
+
     public void getCartItems() {
 
-        for ( Flower key : map.keySet() ) {
-            cartlist.add(key.getName()+" x"+map.get(key));
+        for (Flower key : map.keySet()) {
+            cartlist.add(key.getName() + " x" + map.get(key));
         }
         list.getItems().addAll(cartlist);
     }
-
 
 
     @FXML
@@ -50,12 +61,12 @@ public class OrderBoundary {
         str.getItems().addAll(liststr);
         str.setValue("none");
         String pricetxt;
-        int p=0;
-        for ( Flower key : map.keySet() ) {
-            p +=key.getPrice()*map.get(key);
+        int p = 0;
+        for (Flower key : map.keySet()) {
+            p += key.getPrice() * map.get(key);
         }
 
-        pricetxt =   p + " $";
+        pricetxt = p + " $";
         finalprice.setText(pricetxt);
 
 
@@ -76,5 +87,32 @@ public class OrderBoundary {
     @FXML
     public void returnfunc(javafx.event.ActionEvent actionEvent) throws IOException {
         App.setRoot("catalogboundary");
+    }
+
+    @FXML
+    public void confirmorder(ActionEvent actionEvent) throws IOException {
+
+//        List<Flower> lst = flowerController.getAllData(Flower.class);
+//        ArrayList<Object> answers = new ArrayList<>();
+//        ArrayList<ArrayList<Object>> newarr = new ArrayList<>();
+//        // 1 for name, 2 for id, 3 for price, 4 for sale
+//        // 5 for discount, 6 for color, 7 for type, 8 for image url
+//        for (int i = 0; i < lst.size(); i++) {
+//            newarr.add(new ArrayList<>());
+//            (newarr.get(i)).add(lst.get(i).getName());
+//            (newarr.get(i)).add(lst.get(i).getId());
+//            (newarr.get(i)).add(lst.get(i).getPrice());
+//            (newarr.get(i)).add(lst.get(i).getSale());
+//            (newarr.get(i)).add(lst.get(i).getDiscount());
+//            (newarr.get(i)).add(lst.get(i).getColor());
+//            (newarr.get(i)).add(lst.get(i).getType());
+//            (newarr.get(i)).add(lst.get(i).getImageurl());
+//        }
+//        answers.add("#addOrder");
+//        newarr.add(answers);
+//        //if we added more than one catalog we must change 1
+//        App.getClient().sendToServer(newarr);
+//
+
     }
 }
--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Order.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Order.java
@@ -1,6 +1,7 @@
 package il.cshaifasweng.OCSFMediatorExample.entities;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 @Entity
@@ -11,13 +12,36 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String card;
+    private int userId;
+    private String formOfOrder;
+    private double finalPrice;
+    private String paymentMethod;
     private Date date;
     private String pickup;
+    private String status;
+    private int shopId;
+    @OneToMany
+    private ArrayList<Flower> flowers;
 
-    public Order( String card, Date date, String pickup) {
+    public Order(String card, int userId, String formOfOrder, double finalPrice, String paymentMethod, Date date, String pickup, String status, int shopId, ArrayList<Flower> flowers) {
+        this.card = card;
+        this.userId = userId;
+        this.formOfOrder = formOfOrder;
+        this.finalPrice = finalPrice;
+        this.paymentMethod = paymentMethod;
+        this.date = date;
+        this.pickup = pickup;
+        this.status = status;
+        this.shopId = shopId;
+        this.flowers = new ArrayList<>();
+    }
+
+    public Order(String card, Date date, String pickup) {
+        this.flowers=new ArrayList<>();
         this.card = card;
         this.date = date;
         this.pickup = pickup;
+
     }
 
     public Order() {
