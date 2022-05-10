7848f3b053512274b9c5fdb6d48b542dff0a654c
yaelN21 <yael1234n@gmail.com>
2022-05-10 12:28:18 +0300

    Update complaint.java

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/complaint.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/complaint.java
@@ -15,74 +15,60 @@ public class Compleint{
     private localDate date;
     private String answer;
 
-    public Compleint(String name, String identificationNumber, String email, String phone, String credit,
-                String monthAndYear, String cvv, String password, String account, String storeOrNull) {
-        this.userName = name;
-        this.identificationNumber = identificationNumber;
-        this.email = email;
-        this.phone = phone;
-        this.credit = credit;
-        this.monthAndYear = monthAndYear;
-        this.cvv = cvv;
-        this.password = password;
-        this.account = account;
-        this.storeOrNull = storeOrNull;
-    }
-
-    public Compleint(int id, String orderId, String content, boolean status, Date date, String answer) {
-        this.id = id;
-        this.orderId = orderId;
-        this.content = content;
-        this.status = status;
-        this.date = date;
-        this.answer = answer;
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public int getId() {
-        return id;
-    }
-
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getContent() {
         return content;
     }
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public boolean isStatus() {
         return status;
     }
 
-    public Date getDate() {
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public localDate getDate() {
         return date;
+    }
+
+    public void setDate(localDate date) {
+        this.date = date;
     }
 
     public String getAnswer() {
         return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Compleint(int id, String orderId, String content, boolean status, localDate date, String answer) {
+        this.id = id;
+        this.orderId = orderId;
+        this.content = content;
+        this.status = status;
+        this.date = date;
+        this.answer = answer;
     }
 }
