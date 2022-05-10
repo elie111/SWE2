0da269c9d2237259cef4a247c699b0210544b38f
yaelN21 <yael1234n@gmail.com>
2022-05-10 12:20:46 +0300

    complaint fxml + controller

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/fileCompleintController.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class fileCompleintController {
+    @FXML
+    private Label complaintLable;
+
+    @FXML
+    private TextField complaintTF;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private Button cancleBtn;
+
+    @FXML
+    private Label orderNumberLable;
+
+    @FXML
+    private TextField orderNumberTF;
+
+    @FXML
+    private Label orderNumEmpty;
+
+    @FXML
+    private Label invalidInputOrNum;
+
+    @FXML
+    private Label compEmpty;
+
+
+//    Complaint complaint = new Complaint(); //create complaint in db
+
+    @FXML
+    public void onCLickCancel(ActionEvent event) throws IOException {
+        App.setRoot("catalogBoundary");
+    }
+
+    //check that order number is included only numbers
+    private boolean isValidOrderNum (TextField fieldA){
+        for (int i = 0; i<fieldA.getText().length() ; i++){
+            char temp = fieldA.getText().charAt(i);
+            if(!((temp >= '0') && (temp <= '9'))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //check that relevant fields are not empty
+    private boolean inputCheck (){
+        boolean retVal = true;
+
+        if (complaintTF.getText().isEmpty())
+        {
+            compEmpty.setVisible(true);
+            retVal = false;
+        }
+        return retVal;
+    }
+
+    //if field is empty or invalid show massage - called when sendComplaint is called
+    private void restVisible() {
+        orderNumEmpty.setVisible(false);
+        compEmpty.setVisible(false);
+        invalidInputOrNum.setVisible(false);
+    }
+
+
+    @FXML
+    public void onCLickSend(ActionEvent event) throws IOException {
+        // user put username and password that are not only spaces or null
+        String orderNumber = orderNumberTF.getText();
+        String content = complaintTF.getText();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate today = LocalDate.parse(LocalDate.now().toString(), formatter);
+
+        // write to database
+        ArrayList<Object> arr = new ArrayList<>();
+        arr.add("#complaint");
+        arr.add(orderNumber); //order number
+        arr.add(content); //content of complaint
+        arr.add(true); //status
+        arr.add(today); //the date of the complaint
+        App.getClient().sendToServer(arr);
+    }
+
+
+
+}
---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/complaint.java
@@ -0,0 +1,88 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "complaint")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String orderId;
+    private String content;
+    private boolean status;
+    private localDate date;
+    private String answer;
+
+    public User(String name, String identificationNumber, String email, String phone, String credit,
+                String monthAndYear, String cvv, String password, String account, String storeOrNull) {
+        this.userName = name;
+        this.identificationNumber = identificationNumber;
+        this.email = email;
+        this.phone = phone;
+        this.credit = credit;
+        this.monthAndYear = monthAndYear;
+        this.cvv = cvv;
+        this.password = password;
+        this.account = account;
+        this.storeOrNull = storeOrNull;
+    }
+
+    public User(int id, String orderId, String content, boolean status, Date date, String answer) {
+        this.id = id;
+        this.orderId = orderId;
+        this.content = content;
+        this.status = status;
+        this.date = date;
+        this.answer = answer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
